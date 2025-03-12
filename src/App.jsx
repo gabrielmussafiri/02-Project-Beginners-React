94f855937cd5c0e4231132405201e48e7da19ac6
gabrielmussafiri <musgabriel2@gmail.com>
2025-03-12 14:28:13 +0200

    calculator project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Counter from './components/Counter'
-import Todo from './components/Todo'
-import Meals from './components/Meals'
+import React from "react";
+import Counter from "./components/Counter";
+import Todo from "./components/Todo";
+import Meals from "./components/Meals";
+import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <div>
       {/* <Counter/> */}
       {/* <Todo/> */}
-      <Meals/>
+      {/* <Meals/> */}
+      <Calculator />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
