3eb30832f3e9b75ecd27439844b1c587521f504b
gabrielmussafiri <musgabriel2@gmail.com>
2025-03-12 11:55:35 +0200

    todos completed

---
+++ src/components/Todo.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import "../style_todos.css"
+
+
+const Todo = () => {
+    const defaultTodos = [
+        {
+            text: "Learn React",
+            id: 1
+        },
+        {
+            text: "Learn Firebase",
+            id: 2
+        },
+        {
+            text: "Learn GraphQL",
+            id: 3
+        }
+    ]
+    
+    const [todos , setTodos]= useState([])
+    const[input , setInput]= useState("")
+
+    
+
+
+    const handleSubmit =()=>{
+
+        setTodos((todos) => [
+            ...todos,
+            { 
+                text: input, 
+                id: Math.floor(Math.random() * 1000) 
+            }
+        ]);
+        
+        
+        
+        setInput("")
+    }
+    const removeTodo =(id) =>{
+
+        setTodos((todos) => todos.filter((todo) => todo.id !== id))
+    }
+    
+
+
+
+  return (
+    <div className='container'>
+        <input type="text" placeholder='New Todo Here' value={input} onChange={e =>setInput(e.target.value)} />
+      
+        <button onClick={handleSubmit}>Submit</button>
+        <ul className="todos-list">
+            {todos.map(({text ,id})=>(
+                <li className='todo' key={id}>
+                    <span>{text}</span>
+                    <button className='close' onClick={()=>removeTodo(id)}>x</button>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Todo
